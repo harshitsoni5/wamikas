eb0329736bbd9e1bbb5ccf3c20dda9ec5167dfb6
amantya <amantya@amantyas-MacBook-Pro.local>
2024-05-23 00:16:54 -0700

    setup done on mac

--- lib/Utils/Style/style.dart
+++ lib/Utils/Style/style.dart
@@ -11,33 +11,32 @@ class Styles {
       focusColor: isDarkTheme ? Colors.black54 : const Color(0xffA8DAB5),
       disabledColor: Colors.grey,
       textTheme: Theme.of(context).textTheme.copyWith(
-            headline6: TextStyle(
+            titleLarge: TextStyle(
               color: isDarkTheme ? Colors.white : Colors.black54,
               fontSize: 16.0, // You can adjust the font size as needed
               fontWeight: FontWeight.bold,
             ),
-            headline1: TextStyle(
+            displayLarge: TextStyle(
               color: isDarkTheme ? Colors.white : Colors.black54,
               fontSize: 10.0, // You can adjust the font size as needed
               fontWeight: FontWeight.bold,
             ),
-            headline2: TextStyle(
+            displayMedium: TextStyle(
               color: isDarkTheme ? Colors.white : Colors.black54,
               fontSize: 14.0, // You can adjust the font size as needed
               fontWeight: FontWeight.bold,
             ),
-            headline4: TextStyle(
+            headlineMedium: TextStyle(
               color: isDarkTheme ? Colors.white : Colors.black54,
               fontSize: 12.0, // You can adjust the font size as needed
               fontWeight: FontWeight.bold,
             ),
-            headline5: TextStyle(
+            headlineSmall: TextStyle(
               color: isDarkTheme ? Colors.white : Colors.black54,
               fontSize: 15.0, // You can adjust the font size as needed
               fontWeight: FontWeight.bold,
             ),
           ),
-      backgroundColor: isDarkTheme ? Colors.black54 : Colors.white,
       cardColor: isDarkTheme ? const Color(0xFF151515) : Colors.white,
       canvasColor: isDarkTheme ? Colors.black54 : Colors.grey[50],
       buttonTheme: Theme.of(context).buttonTheme.copyWith(
@@ -47,10 +46,9 @@ class Styles {
       appBarTheme: const AppBarTheme(
         elevation: 0.0,
       ),
-      colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red)
-          .copyWith(background: isDarkTheme ? Colors.black54 : Colors.white),
       textSelectionTheme: TextSelectionThemeData(
-          selectionColor: isDarkTheme ? Colors.white : Colors.black54),
+          selectionColor: isDarkTheme ? Colors.white : Colors.black54), colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red)
+          .copyWith(background: isDarkTheme ? Colors.black54 : Colors.white).copyWith(background: isDarkTheme ? Colors.black54 : Colors.white),
     );
   }
 }
